ecb60e2f12fcc204b1c42be5269e5673d6bcc01d
Fereshta Alavy <fereshta.alavy@sjsu.edu>
2020-03-01 20:04:14 -0800

    add front-end code

---
+++ get_together-frontend/src/components/button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function button({text}){
+    return (
+        <>
+        <button>text</button>
+        <label>press here</label>
+        </>
+    )
+}
+
+export default button;
