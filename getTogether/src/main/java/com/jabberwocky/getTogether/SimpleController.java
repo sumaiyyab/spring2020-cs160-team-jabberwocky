88bdfbea1c952cfeb705e9164a881b87546d54eb
Fereshta Alavy <alavy.fereshta@gmail.com>
2020-03-02 16:51:24 -0800

    connect front-end to back-end

--- getTogether/src/main/java/com/jabberwocky/getTogether/SimpleController.java
+++ getTogether/src/main/java/com/jabberwocky/getTogether/SimpleController.java
@@ -3,11 +3,13 @@ package com.jabberwocky.getTogether;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SimpleController {	
+public class SimpleController {
+    @CrossOrigin
     @GetMapping("/sprint2")
     public String homePage() {
         return "home";
