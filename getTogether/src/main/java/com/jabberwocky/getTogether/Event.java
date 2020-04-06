9bbd0480cc7a2d0d2a92f636bd16fa0d1ace0b17
Sumaiyya Burney <33647766+sumaiyyab@users.noreply.github.com>
2020-04-06 04:34:08 -0700

    Adds findBestTimes() functionality
    given list of users to invite, find the free time that works for the
    most possible users

--- getTogether/src/main/java/com/jabberwocky/getTogether/Event.java
+++ getTogether/src/main/java/com/jabberwocky/getTogether/Event.java
@@ -3,7 +3,8 @@ package com.jabberwocky.getTogether;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.UUID;
+import java.util.Collections;
+import java.util.HashMap;
 
 import org.springframework.data.annotation.Id;
 
@@ -13,53 +14,85 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Event {
-	
+
 	@Id
 	private String id;	
-	private String userID;
-	private User host;
+	private String hostID;
 	public String title;
 	private LocalDateTime startTime;
 	private LocalDateTime endTime;
-	private LocalDate startDate;
-	private LocalDate endDate;
+	private int duration; //mins
+	private LocalDate startPossible;
+	private LocalDate endPossible;
 	private String location;
 	private ArrayList<String> tags;
 	private ArrayList<User> invited;
 	private ArrayList<User> rsvp;
-	
-	
-	public Event(String userID, String title, String location, ArrayList<String> tags, ArrayList<User> invited, LocalDate startDate, LocalDate endDate) {
-		this.userID = userID;
+
+
+	public Event(String userID, String title, String location, ArrayList<String> tags, ArrayList<User> invited, LocalDateTime startTime, LocalDateTime endTime) {
+		//the Actual filled out event; the only kind of event that should be saved to repo
+		this.hostID = userID;
 		this.title = title;
-		//this.startTime = startTime;
-		//this.endTime = endTime;
 		this.location = location;
 		if(tags == null) {
 			this.tags = new ArrayList<>();
 		}else {
 			this.tags = tags;
 		}
-		
+
 		this.rsvp = new ArrayList<>();
-		
+
 		// Created invited arrayList for people who haven't rsvpsd
 		this.invited = invited;
-		
-		this.startDate = startDate;
-		this.endDate = endDate;
+
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+
+	public Event(String userID, String title, ArrayList<User> invited, LocalDate startPossible, LocalDate endPossible, int duration) { 
+		//used when first creating event, before searching
+		this.startPossible = startPossible;
+		this.endPossible = endPossible;
+		this.duration = duration;
 	}
 	
-	public void setHost(User person) {
-		host = person;
-		rsvp.add(person);
+	public Event(LocalDateTime startTime, LocalDateTime endTime) { //used when presenting potential events that could fit free time
+		this.startTime = startTime;
+		this.endTime = endTime;
 	}
-	
+
 	public void addAttendee(User user) {
 		rsvp.add(user);
 	}
-	
-	public void findTimes() {
-		// go through the rsvp list and find times within the dates
+
+	public boolean hasOverlapWith(LocalDateTime start2, LocalDateTime end2) {
+		return !this.endTime.isBefore(start2) && !this.startTime.isAfter(end2);
+	}
+
+	public ArrayList<Event> findTimes() {
+		// go through the invited list and find times within the dates
+		LocalDateTime possibleStartTime = startPossible.atStartOfDay();
+		LocalDateTime possibleEndTime = possibleStartTime.plusMinutes(duration);
+		HashMap<LocalDateTime, Integer> timeslots = new HashMap<LocalDateTime, Integer>();
+		while(possibleEndTime.isBefore(endPossible.atTime(11, 59))) { //O(n^2) gross >_<
+			int freeUsers = 0;
+			for (User i:invited) {
+				if(i.isFreeAt(possibleStartTime, duration)) {
+					freeUsers++;
+				}
+			}
+			timeslots.put(possibleStartTime, freeUsers);
+			possibleStartTime = possibleEndTime;
+			possibleEndTime = possibleStartTime.plusMinutes(duration);
+		}
+		int maxValueInMap=(Collections.max(timeslots.values()));  // This will return max value in the Hashmap
+		ArrayList<Event> bestTimes = new ArrayList<>();
+        for (LocalDateTime key : timeslots.keySet()) {  // Itrate through hashmap
+            if (timeslots.get(key)==maxValueInMap) {
+                bestTimes.add(new Event(key, key.plusMinutes(duration)));     // Print the key with max value
+            }
+        }
+		return bestTimes;
 	}
 }
