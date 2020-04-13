72f7c80d2082f39918b80c2a2234beae58c109a3
theCuriousMan <38036270+theCuriousMan@users.noreply.github.com>
2020-04-13 14:36:26 -0700

    Adding signUp to the frontend

--- get_together-frontend/src/App.js
+++ get_together-frontend/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import SignUp from './signUp';
 import './App.css';
 import Login from "./login"
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
   Link
 } from "react-router-dom";
 import Scheduler from './Scheduler';
@@ -13,10 +16,16 @@ function App(){
   
   return (
     <Router>
+     
       <Switch>
           <Route path="/login">
+
             <Login />
+
           </Route>
+        <Route path= "/signUp">
+              <SignUp />
+            </Route>
           <Route path="/dashboard">
             <Scheduler/>
           </Route>
